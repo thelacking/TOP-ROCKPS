bc001a12c632c8b55fdcf6ade6299c842fca5ffa
cj <27069314+thelacking@users.noreply.github.com>
2025-02-09 14:36:25 -0800

    created a ui

--- rps.js
+++ rps.js
@@ -1,17 +1,56 @@
-let UserAnwser = prompt("pease choose rock paper or sizzors");
+
+
 function getComAnswer(){
     let rn = Math.floor(Math.random() * 3);
     let rpsword = "";
-    if(rn === 1){
+    if(rn === 0){
         rpsword = "rock";
     }
-    if(rn === 2){
+    if(rn === 1){
         rpsword = "paper";
     }
-    if(rn === 3){
-        rpsword = "sizzors";
+    if(rn === 2){
+        rpsword = "scissors";
     }
     return rpsword
 }
-let computerAnswer = getComAnswer();
-console.log(computerAnswer);
+
+function userRock(){
+    let UserAnwser = 'rock';
+    let computerAnswer = getComAnswer();
+    if(UserAnwser === computerAnswer){
+        document.getElementById("results").innerHTML = "its a draw. you both chose " + computerAnswer;
+    }
+    else if(UserAnwser === 'rock' & computerAnswer === 'paper'){
+        document.getElementById("results").innerHTML = "You lose. computer chose: " + computerAnswer;
+    }
+    else if(UserAnwser === 'rock' & computerAnswer === 'scissors'){
+        document.getElementById("results").innerHTML =" You win. computer chose: " + computerAnswer;
+    }
+}
+function userPaper(){
+    let UserAnwser = 'paper';
+    let computerAnswer = getComAnswer();
+    if(UserAnwser === computerAnswer){
+        document.getElementById("results").innerHTML = "its a draw. you both chose " + computerAnswer;
+    }
+    else if(UserAnwser === 'paper' & computerAnswer === 'scissors'){
+        document.getElementById("results").innerHTML = "You lose. computer chose: " + computerAnswer;
+    }
+    else if(UserAnwser === 'paper' & computerAnswer === 'rock'){
+        document.getElementById("results").innerHTML =" You win. computer chose: " + computerAnswer;
+    }
+}
+function userScissors(){
+    let UserAnwser = 'scissors';
+    let computerAnswer = getComAnswer();
+    if(UserAnwser === computerAnswer){
+        document.getElementById("results").innerHTML = "its a draw. you both chose " + computerAnswer;
+    }
+    else if(UserAnwser === 'scissors' & computerAnswer === 'rock'){
+        document.getElementById("results").innerHTML = "You lose. computer chose: " + computerAnswer;
+    }
+    else if(UserAnwser === 'scissors' & computerAnswer === 'paper'){
+        document.getElementById("results").innerHTML =" You win. computer chose: " + computerAnswer;
+    }
+}
